672effc3a4abe1089c8c3b62cdcea1736c60d764
Erla Sóldís Þorbergsdóttir <erlasoldis@gmail.com>
2019-09-05 19:37:39 +0000

    Started implementation

--- sequence.py
+++ sequence.py
@@ -3,6 +3,16 @@
 # Algorithm
 #1. Get input for the length of the sequence
 #2. Start the sequence with 1
-#3. Calculate the next number in the sequence and print it out in the sequence in the format 1, 2, 3, 6, 11, 20, 37, ___, ...
-#4. Repeat step 3 until the input length of the sequence is reached
+#3. After three numbers in the sequence, the next number is the sum of the three previous numbers.
+#4. Stop when the sequence length reaches the input number
+#4. Print the sequence in the format 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
+
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+seq_number = 1
+
+print ("1, 2, 3, ", end='')
+
+
 
