c2db96bbb8ab969c334544c3678787a57a84bfe2
Erla Sóldís Þorbergsdóttir <erlasoldis@gmail.com>
2019-09-05 19:26:38 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,8 @@
+#Design an algorithm that generates the first n numbers in the following sequence:; 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
+
+# Algorithm
+#1. Get input for the length of the sequence
+#2. Start the sequence with 1
+#3. Calculate the next number in the sequence and print it out in the sequence in the format 1, 2, 3, 6, 11, 20, 37, ___, ...
+#4. Repeat step 3 until the input length of the sequence is reached
+
