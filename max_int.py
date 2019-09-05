43c9fb6affe9705c47d8ffc3d94be8a219b26625
Erla Sóldís Þorbergsdóttir <erlasoldis@gmail.com>
2019-09-05 18:57:59 +0000

    Added loops

---
+++ max_int.py
@@ -0,0 +1,19 @@
+#Maximum number
+
+#1. Prompt the user for and input
+#2. Prompt for another input
+#3. Compare the last 2 inputs and store the greater of the two
+#4. Repeat steps 2 and 3
+#5. Stop if the input is negative
+#6. Print the maximum input
+
+
+num_int = int(input("Input a number: "))
+
+max_int = num_int
+
+#Continue if input is a positive number
+if num_int > 0 :
+    
+    #Repeat the while loop while input is a positive number
+    while num_int > 0 :
