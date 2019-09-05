60780631435cf77316deba84150193fcf182ab91
Erla Sóldís Þorbergsdóttir <erlasoldis@gmail.com>
2019-09-05 20:10:36 +0000

    Solution

--- sequence.py
+++ sequence.py
@@ -1,3 +1,4 @@
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
 #Design an algorithm that generates the first n numbers in the following sequence:; 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
 
 # Algorithm
@@ -5,14 +6,32 @@
 #2. Start the sequence with 1
 #3. After three numbers in the sequence, the next number is the sum of the three previous numbers.
 #4. Stop when the sequence length reaches the input number
-#4. Print the sequence in the format 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
+#4. Print the sequence
 
 
-n = int(input("Enter the length of the sequence: ")) # Do not change this line
+n1 = 1
+n2 = 2
+n3 = 3
 
-seq_number = 1
 
-print ("1, 2, 3, ", end='')
+for i in range (1, n+1) :
+
+    if i == 1 :
+        print(n1)
+
+    elif i == 2 :
+        print(n2)
+
+    elif i == 3 :
+        print(n3)
+    
+    else :
+        next_number = n1+n2+n3
+        print(next_number)
+        n1 = n2
+        n2 = n3
+        n3 = next_number
+
 
 
 
