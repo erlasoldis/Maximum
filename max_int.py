53953d268f8b285d7961bc9e7811f2c5fb4f5036
Erla Sóldís Þorbergsdóttir <erlasoldis@gmail.com>
2019-09-05 19:07:26 +0000

    Fixed solution

--- max_int.py
+++ max_int.py
@@ -2,7 +2,7 @@
 
 #1. Prompt the user for and input
 #2. Prompt for another input
-#3. Compare the last 2 inputs and store the greater of the two
+#3. Compare the inputs and store the maximum input
 #4. Repeat steps 2 and 3
 #5. Stop if the input is negative
 #6. Print the maximum input
@@ -10,18 +10,20 @@
 
 num_int = int(input("Input a number: "))
 
-max_int = num_int
+max_int = 0
 
 #Continue if input is a positive number
 if num_int > 0 :
     
     #Repeat the while loop while input is a positive number
     while num_int > 0 :
-
+        
         #Switch out the max_input number when a greater number is input
         if num_int > max_int :
-        max_int = num_int
+            max_int = num_int
 
         num_int = int(input("Input a number: "))
+
+            
     
     print("The maximum is", max_int)
